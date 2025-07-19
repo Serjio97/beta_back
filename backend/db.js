de0c6677dc695907a3a9f7444d3ec18d9d3a29df
Firas Dhaouadi <firasdhaouadi@edtrust.tn>
2025-07-19 16:36:22 +0100

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const db = await mysql.createPool({
-  host: 'localhost',
+  host: 'betawaves_sql',
   user: 'root',
-  password: '',     // replace if needed
-  database: 'betawaves_cms', // make sure this matches your DB name
-  port: 3307,
+  password: '8ef6e905734340e80e8a',     // replace if needed
+  database: 'betawaves', // make sure this matches your DB name
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
